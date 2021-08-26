29f755ab4be0931f2ec412feb0b1fd398188c9bb
hanks010 <hanks010@naver.com>
2021-08-27 02:58:01 +0900

    습관 추가 페이지 1차 완성

--- app/src/main/java/com/cos/better/view/habit/AddHabitActivity.java
+++ app/src/main/java/com/cos/better/view/habit/AddHabitActivity.java
@@ -1,16 +1,67 @@
 package com.cos.better.view.habit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import com.cos.better.R;
+import com.cos.better.config.InitSetting;
+import com.cos.better.view.HomeActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class AddHabitActivity extends AppCompatActivity {
+public class AddHabitActivity extends AppCompatActivity implements InitSetting {
+
+    private static final String TAG = "AddHabitActivity";
+    private Context mContext = this;
+    private Button btnClose, btnBack;
+    private AppCompatButton btnHabitText,btnComplete;
+    private FloatingActionButton btnAddAlarm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit);
+        init();
+        initData();
+        initLr();
+
     }
+
+    @Override
+    public void init() {
+        btnClose = findViewById(R.id.btnClose);
+        btnBack = findViewById(R.id.btnBack);
+        btnHabitText = findViewById(R.id.btnHabitText);
+        btnAddAlarm = findViewById(R.id.btnAddAlarm);
+        btnComplete = findViewById(R.id.btnComplete);
+    }
+
+    @Override
+    public void initLr() {
+        btnClose.setOnClickListener(v ->{
+            Intent intent = new Intent(mContext, HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            mContext.startActivity(intent);
+        });
+        btnBack.setOnClickListener(v ->{
+            finish();
+        });
+
+    }
+
+    @Override
+    public void initData() {
+        Intent intent = getIntent();
+       String categoryName = intent.getStringExtra("categoryName");
+       String category = intent.getStringExtra(categoryName);
+       btnHabitText.setText(category);
+    }
+
+
 }
